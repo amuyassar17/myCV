ff41423909e9441bd67113d5b9e41d540705d4c0
amuyassar17 <amuyassar16@gmail.com>
2020-09-27 02:49:03 +0800

    Updates

---
+++ precache-manifest.663ec5b6981d4b12166a04da50010106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e1d6dae4305d79aba9acd1d66420c7",
+    "url": "/myCV/index.html"
+  },
+  {
+    "revision": "3ca67ca0c36e4ae3e097",
+    "url": "/myCV/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "761e4516e9625fb1cb33",
+    "url": "/myCV/static/css/main.8562fec9.chunk.css"
+  },
+  {
+    "revision": "3ca67ca0c36e4ae3e097",
+    "url": "/myCV/static/js/2.59cd8d6e.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/myCV/static/js/2.59cd8d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761e4516e9625fb1cb33",
+    "url": "/myCV/static/js/main.2c6b2fa0.chunk.js"
+  },
+  {
+    "revision": "93d4c553fcb0c9691830",
+    "url": "/myCV/static/js/runtime-main.b198f060.js"
+  },
+  {
+    "revision": "8f65ed4eed2408b76e81fd7ec46551d2",
+    "url": "/myCV/static/media/ahmad.8f65ed4e.jpg"
+  }
+]);
